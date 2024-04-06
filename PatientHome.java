f93ed0f6918952ad1645e6c822afc8054d704e31
Chistosa <143834239+Chistosa@users.noreply.github.com>
2024-04-05 18:50:01 -0700

    Create PatientHome.java
    Patient Home screen
    What patients see after they login

---
+++ PatientHome.java
@@ -0,0 +1,83 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class PatientHome {
+	public static void display(String patientID) {
+		//get the patients first name and last name to display it on the screen
+		String firstName = "";
+		String lastName = "";
+		try (BufferedReader reader = new BufferedReader(new FileReader(patientID + "_Info.txt"))) 
+		{
+			String line;
+		    while ((line = reader.readLine()) != null) 
+		    {
+		    	if(line.startsWith("First name: "))
+		    		firstName = line.substring(12);
+		    	if(line.startsWith("Last name: "))
+		    		lastName = line.substring(11);
+		    }
+		} catch (IOException e) {
+			e.printStackTrace();
+			}		
+		GridPane pane = new GridPane();
+		pane.setAlignment(Pos.CENTER);
+		pane.setHgap(5);
+		pane.setVgap(5);
+		pane.setPadding(new Insets(25,25,25,25));
+		
+		Label title = new Label("Patient: " + firstName + " " + lastName);
+	    Button visitButton = new Button("Recent Visit");
+	    Button infoButton = new Button("Contact Information");
+        Button logOut = new Button("Log Out");
+	       
+        VBox.setMargin(title, new Insets(20, 0, 80, 0));
+	    VBox layout = new VBox(10);
+	    layout.getChildren().addAll(title, visitButton, infoButton);
+	    layout.setAlignment(Pos.CENTER);
+	        
+        VBox logout = new VBox(10);
+        logout.getChildren().addAll(logOut);
+	    logout.setAlignment(Pos.BOTTOM_CENTER);
+	    
+	    BorderPane borderPane = new BorderPane();
+	    borderPane.setPadding(new Insets(25,25,25,25));
+	    borderPane.setCenter(layout);
+	    borderPane.setBottom(logout);
+
+	    Scene scene = new Scene(borderPane, 500, 500);
+	    Stage window = new Stage();
+		window.setTitle("Patient Home");
+		window.setScene(scene);
+		window.show();
+		
+		//handle button presses
+		visitButton.setOnAction(e -> {
+			Stage stage = (Stage) visitButton.getScene().getWindow();
+			stage.close(); 
+			Summary.display(patientID);
+		});
+		infoButton.setOnAction(e -> {
+			Stage stage = (Stage) infoButton.getScene().getWindow();
+			stage.close(); 
+			UpdatePatientInfoView.display(patientID);
+		});
+		logOut.setOnAction(e -> {
+			Stage stage = (Stage) logOut.getScene().getWindow();
+			stage.close();
+			Login.display(); 
+		});
+	}
+}
